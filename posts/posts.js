6ce1e8452bf77508cca87f13d7f479bee13ad843
Riley Rice <riceriley59@gmail.com>
2022-07-28 12:40:47 -0700

    added responsiveness to nav bar along with sticky nav-bar for scrollable sections I also added name and year made on each page

--- posts/posts.js
+++ posts/posts.js
@@ -1,25 +1,46 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.showcaselist');
-    const showcaselinks = document.querySelectorAll('.showcaselist li');
+const burger = document.querySelector('.burger');
+const showcaselinks = document.querySelectorAll('#showcaselist li');
+const header = document.getElementById('showcase');
 
-    burger.addEventListener('click', ()=>{
-        nav.classList.toggle('nav-active');
+burger.addEventListener('click', ()=>{
+    showcaselist.classList.toggle('nav-active');
 
 
-        showcaselinks.forEach((link, index) => {
-            link.style.animation = 'navLinkFade 0.5s ease forwards ' + (index/7) + 's';
-        });
-
-        burger.classList.toggle('toggle');
-
-        showcaselinks.forEach((link) =>{
-            link.addEventListener('click', ()=>{
-                showcaselist.classList.toggle('nav-active');
-                burger.classList.remove('toggle');
-            });
-        });
+    showcaselinks.forEach((link, index) => {
+        link.style.animation = 'navLinkFade 0.5s ease forwards ' + (index/7) + 's';
     });
-}
 
-navSlide();
+    burger.classList.toggle('toggle');
+
+    if(window.scrollY < 20 && window.innerWidth > 768 && window.innerWidth < 1024){
+        header.classList.toggle("sticky");
+    }
+    else if(window.scrollY < 20 && window.innerWidth < 640){
+        header.classList.toggle("sticky");
+    }
+});
+
+showcaselinks.forEach((link) =>{
+    link.addEventListener('click', ()=>{
+        showcaselist.classList.toggle('nav-active');
+        burger.classList.remove('toggle');
+    });
+});
+
+window.addEventListener('scroll', ()=>{
+	let value = window.scrollY;
+
+	//sticky and responsive nav-bar
+	if(window.innerWidth > 1600){
+		header.classList.toggle("sticky", value > 50);
+	}
+	else if(window.innerWidth > 1200 && window.innerWidth < 1600){
+		header.classList.toggle("sticky", value > 40);
+	}
+	else if(window.innerWidth > 768 && window.innerWidth < 1024){
+		header.classList.toggle("sticky", value > 20);
+	}
+	if(window.innerWidth < 640){
+		header.classList.toggle("sticky", value > 20);
+	}
+});
